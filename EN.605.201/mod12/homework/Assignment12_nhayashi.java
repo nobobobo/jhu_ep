ea10d0adcf6c7f2600713689bb648d9a96d9e115
nobobobo <ianhsu1221@gmail.com>
2020-08-08 21:30:31 -0700

    comments & capture

--- EN.605.201/mod12/homework/Assignment12_nhayashi.java
+++ EN.605.201/mod12/homework/Assignment12_nhayashi.java
@@ -8,15 +8,20 @@ import javafx.geometry.*;
 
 public class Assignment12_nhayashi extends Application{
     public void start (Stage myStage){
+        // Set title and Resizable:false for the stage
         myStage.setTitle("Simple Calculator");
         myStage.setResizable(false);
+
+        // init and setup the grid pane
         GridPane rootNode = new GridPane();
         rootNode.setHgap(5);
         rootNode.setVgap(5);
         rootNode.setAlignment(Pos.CENTER);
 
+        // create a scene
         Scene myScene = new Scene(rootNode, 300, 200);
 
+        // define labels and text fields
         Label num1Label = new Label("First Value: ");
         Label num2Label = new Label("Second Value: ");
         Label sumLabel = new Label("Sum is: ");
@@ -26,8 +31,10 @@ public class Assignment12_nhayashi extends Application{
         TextField sumField = new TextField();
         sumField.setEditable(false);
 
+        // create a button
         Button aButton = new Button("Calculate");
 
+        // add nodes to the grid pane by indices
         rootNode.add(num1Label, 0,0);
         rootNode.add(num1Field, 1,0);
         rootNode.add(num2Label, 0,1);
@@ -36,8 +43,11 @@ public class Assignment12_nhayashi extends Application{
         rootNode.add(sumField, 1,2);
 
         rootNode.add(aButton,1,3);
+        
+        // set asslignment for the button
         rootNode.setHalignment(aButton, HPos.RIGHT);
         
+        // setting the scene to the stage and show.
         myStage.setScene(myScene);
         myStage.show();
     }
