eb4186e0ac7cc4a3d8e475c682d5732edfe3330d
nobobobo <ianhsu1221@gmail.com>
2021-05-02 22:59:05 -0700

    p2

--- EN.685.621_Algorithms_for_Data_Science/pa2/p1/p1.py
+++ EN.685.621_Algorithms_for_Data_Science/pa2/p1/p1.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd 
-import scipy as sp
-from scipy.stats import chi2
+
 
 # libraries for feature selections 
 from sklearn.feature_selection import SelectKBest
@@ -10,6 +9,20 @@ from sklearn.feature_selection import f_classif
 
 # library for data preprocessing 
 from sklearn.preprocessing import normalize
+import scipy as sp
+from scipy.stats import chi2
+
+
+# classifier model libraries 
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.svm import SVC
+
+
+# libary for 5-fold
+from sklearn.model_selection import cross_val_score
 
 
 def selectFeatures(X, y, k=10):
@@ -49,6 +62,63 @@ def removeOutliers(class_data, outlier):
     return [class_data[i] for i in remain]
 
 
+def bayes(X, y): 
+    gnb = GaussianNB().fit(X,y)
+    y_pred = gnb.predict(X)
+    tot = len(y) 
+    acc = 1.0* (y==y_pred).sum() / tot
+    print('Accuracy of Naive Bayes Classifier is: ', acc)
+
+
+def lda(X, y): 
+    clf = LinearDiscriminantAnalysis().fit(X, y)
+    y_pred = clf.predict(X)
+    tot = len(y) 
+    acc = 1.0* (y==y_pred).sum() / tot
+    print('Accuracy of LDA is: ', acc)
+
+
+def rbfnn(X, y): 
+    kernel = 1.0 * RBF(1.0)
+    gpc = GaussianProcessClassifier(kernel=kernel,random_state=1).fit(X,y)
+    print(gpc.score(X,y))
+    # y_pred = gpc.predict(X)
+    # tot = len(y) 
+    # acc = 1.0* (y==y_pred).sum() / tot
+    # print('Accuracy of RBFNN is: ', acc)    
+
+
+def sup_vec_mac(X, y): 
+    kernel = 'poly'
+    clf = SVC(kernel=kernel, gamma='auto').fit(X,y)
+    y_pred = clf.predict(X)
+    tot = len(y) 
+    acc = 1.0* (y==y_pred).sum() / tot
+    print('Accuracy of SVM is: ', acc)    
+    
+
+def five_fold_1(X,y):
+    clf = GaussianNB()
+    scores = cross_val_score(clf, X, y, cv=5)
+    print("Bayes with normalization & outlier removal: ")
+    print("%0.2f accuracy with a standard deviation of %0.2f\n" % (scores.mean(), scores.std()))
+
+def five_fold_2(X,y):
+    clf = GaussianNB()
+    scores = cross_val_score(clf, X, y, cv=5)
+    print("Bayes with normalization: ")
+    print("%0.2f accuracy with a standard deviation of %0.2f\n" % (scores.mean(), scores.std()))
+
+def five_fold_3(X,y):
+    clf = GaussianNB()
+    scores = cross_val_score(clf, X, y, cv=5)
+    print("SVM(Poly) with normalization: ")
+    print("%0.2f accuracy with a standard deviation of %0.2f\n" % (scores.mean(), scores.std()))
+
+    
+
+
+
 
 if __name__ == '__main__': 
     df = pd.read_csv('trainFeatures42k.csv', header=None)
@@ -67,22 +137,21 @@ if __name__ == '__main__':
         tmp_X = tmp_X + new_class_data
         tmp_y = tmp_y + cls.tolist()
 
-    X_new = pd.DataFrame(tmp_X)
-    y_new = pd.DataFrame(tmp_y)
+    X_rmv = pd.DataFrame(tmp_X)
+    y_rmv = pd.Series(tmp_y)
     
-    output = X_new 
-    output['class'] = y_new
-
+    output = X_rmv
+    output['class'] = y_rmv
     output.to_csv('processed_train.csv')
-    # X_new = pd.DataFrame(tmp_X[0])
-    # y_new = pd.DataFrame(tmp_y[0])
 
-    # print(X_new)
-    # test = pd.DataFrame(tmp_X[1])
-    # print(test)
-    # X_new.append(test, ignore_index = True)
-    # print(X_new) 
 
-    
-    # print(len(tmp_y))
+    bayes(X_rmv, y_rmv)
+    lda(X_rmv, y_rmv)
+    # rbfnn(X_new, y_new)
+    sup_vec_mac(X_rmv,y_rmv)
+
+    five_fold_1(X_rmv,y_rmv)
+    five_fold_2(X_new,y)
+    five_fold_3(X_new,y)
+
     print('done')
